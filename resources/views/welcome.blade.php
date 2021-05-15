9bf93c84832f6a3bdd8fde7e2050395567bce1f4
Peter Ncube <pmn@outlook.com>
2021-05-15 06:03:34 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,226 @@
+
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+
+	<head>
+	
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<!-- Title -->
+		<title>Medisah - Coming Soon</title>
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico">
+		
+		<!-- Font awesome -->
+		<link rel="stylesheet" type="text/css" href="dcss/all.min.css" />
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+		<!-- Google web fonts -->
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,700,700italic">
+		
+		<!-- Stylesheet -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/media.css">
+		
+		<!-- Color schema -->
+		<link rel="stylesheet" type="text/css" href="css/color.css" class="colors">
+		
+		<!-- YTPlayer -->
+		<link rel="stylesheet" type="text/css" href="css/ytplayer.min.css">
+		
+		<!-- Settings (Remove it on your site) -->
+		<link rel="stylesheet" type="text/css" href="css/settings.css">
+	
+	</head>
+	
+	<body>
+		<div class="wrap" id="bg-image">
+	
+			<!-- Main -->
+			<div id="main">
+				<div class="inner">
+	
+					<!-- Header -->
+					<header>
+						<h1 class="logo fade-in">
+							<img src="img/logo.jpeg" width="350" height="200" alt="Medisah" />
+						</h1>
+					</header>
+	
+					<!-- Content -->
+					<section class="content">
+	
+						<h1 class="title">
+							<span>Coming soon!</span>
+						</h1>
+						
+						<p class="slogan">
+							Medicine dispensing and stock tracking will never be the same again. We are building a solution to all pharmacy problems. <br />
+							One day at a time.
+						</p>
+	
+						<!-- Countdown timer -->
+						<div id="timer"></div>
+	
+						<p class="subtitle">Notify Me When It's Ready</p>
+	
+						<!-- Newsletter form -->
+						<div id="newsletter" class="form-wrap">
+						
+							<form action="subscribe.php" method="post" id="newsletter-form">
+							
+								<p class="form-field">
+									<input type="text" name="email" id="email" value="" placeholder="Your email" />
+								</p>
+								
+								<p class="form-submit">
+									<input type="submit" name="submit" id="submit" value="Subscribe" />
+								</p>
+								
+							</form>
+							
+						</div>
+	
+						<!-- Social links -->
+						<div class="social">
+							<ul>
+								
+								<li>
+									<a href="#" title="Twitter">
+										<i class="fab fa-twitter"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#" title="Facebook">
+										<i class="fab fa-facebook-f"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#" title="Pinterest">
+										<i class="fab fa-pinterest"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#" title="Instagram">
+										<i class="fab fa-instagram"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#" title="Vimeo">
+										<i class="fab fa-vimeo-v"></i>
+									</a>
+								</li>
+							
+							</ul>
+						</div>
+	
+					</section>
+	
+					<!-- Modal page toggle -->
+					<div class="modal-toggle">
+						<a href="#" id="modal-open" title="More Info">
+							<i class="fas fa-angle-right"></i>
+						</a>
+					</div>
+	
+				</div>
+			</div>
+	
+			<!-- Modal page: About Us -->
+			<div id="modal">
+				<div class="inner">
+	
+					<!-- Modal toggle -->
+					<div class="modal-toggle">
+						<a href="#" id="modal-close" title="Close">
+							<i class="fas fa-times"></i>
+						</a>
+					</div>
+	
+					<!-- Content -->
+					<section class="content">
+						
+						<h1 class="title">About <span>Selene</span></h1>
+					
+						<!-- Columns -->
+						<div class="row">
+							
+							<div class="one-half">
+								<p>Sed congue blandit augue, at malesuada nisl pulvinar et. Fusce varius magna eget magna maximus venenatis.</p>
+								<p>Nulla facilisi. Pellentesque ac finibus diam. Suspendisse potenti. Nullam id nibh at velit placerat ornare non non lorem.</p>
+								<p>Morbi vulputate sed turpis sed mollis. Ut eget lectus eros. Donec mollis et orci eu vulputate.</p>
+							</div>
+							
+							<div class="one-half">
+								<h2><i class="fas fa-phone"></i> Phone</h2>
+								<p>
+									Phone: +1 (866) 496-3250<br />
+									Fax: +1 (866) 496-3251
+								</p>
+								
+								<h2><i class="fas fa-envelope"></i> Email</h2>
+								<p>
+									<a href="/cdn-cgi/l/email-protection#46232b272f2a06352f322328272b236825292b"><span class="__cf_email__" data-cfemail="8de8e0ece4e1cdfee4f9e8e3ece0e8a3eee2e0">[email&#160;protected]</span></a>
+								</p>
+								
+								<h2><i class="fas fa-map"></i> Address</h2>
+								<p>
+									200 Aquarium Way Long Beach, CA 90803, United States
+								</p>
+							</div>
+							
+						</div>
+	
+					</section>
+	
+				</div>
+			</div>
+	
+		</div>
+	
+		<!-- Background overlay -->
+		<div class="body-bg"></div>
+	
+		<!-- Loader -->
+		<div class="page-loader">
+			<div class="progress">Loading...</div>
+		</div>
+	
+		<!-- jQuery -->
+		<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="layout/plugins/jquery/jquery.js"></script>
+
+		<!-- Plugins -->
+		<script src="layout/plugins/modernizr/modernizr.custom.js"></script>
+		<script src="layout/plugins/backstretch/jquery.backstretch.min.js"></script>	
+		<script src="layout/plugins/plugin/jquery.plugin.min.js"></script>
+		<script src="layout/plugins/countdown/jquery.countdown.min.js"></script>
+		<script src="layout/plugins/validate/jquery.validate.min.js"></script>
+		<script src="layout/plugins/placeholder/jquery.placeholder.min.js"></script>
+		<script src="layout/plugins/ytplayer/jquery.mb.ytplayer.min.js"></script>
+
+		<!-- Main -->
+		<script src="layout/js/main.js"></script>
+
+		<!-- Settings (Remove it on your site) -->
+		<script src="layout/plugins/settings/jquery.cookies.min.js"></script>
+		<script src="layout/plugins/settings/settings.js"></script>
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171793417-4"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-171793417-4');
+        </script>
+	
+	</body>	
+	
+</html>
